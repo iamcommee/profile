321e260e42aa089bb9eab907321c83bf9340fa01
Chaloempong Budkrajang <i.am.commee@gmail.com>
2021-07-05 14:39:14 +0700

    2021-07-05

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
       // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap' },
       { rel: 'stylesheet', href:'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' },
+      { rel: 'stylesheet', href:'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css' },
     ],
     script: [
       { src: 'https://kit.fontawesome.com/bf82e33f0d.js', crossorigin: 'anonymouse' },
